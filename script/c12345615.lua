081b90e9da8ef3ccf8ec351df18de5187b43778d
davidshemla <davidshemla2501@gmail.com>
2024-07-31 02:22:21 +0300

    h

---
+++ script/c12345615.lua
@@ -0,0 +1,109 @@
+--The Seal Of Orichalcos
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+	end
+	e:SetLabel(1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--condition
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local c=e:GetHandler()
+	-- ATK boost
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetValue(500)
+	Duel.RegisterEffect(e1,tp)
+	-- Target restriction
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCondition(s.atkcon)
+	e2:SetValue(s.atklimit)
+	Duel.RegisterEffect(e2,tp)
+	-- Indestructible count
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e3:SetTargetRange(LOCATION_ONFIELD,0)
+	e3:SetValue(s.indct)
+	Duel.RegisterEffect(e3,tp)
+	-- Draw a card when losing LP or paying LP
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_DAMAGE)
+	e4:SetCondition(s.drcon)
+	e4:SetOperation(s.drop)
+	Duel.RegisterEffect(e4,tp)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetCode(EVENT_PAY_LPCOST)
+	e5:SetCondition(s.drcon)
+	e5:SetOperation(s.drop)
+	Duel.RegisterEffect(e5,tp)
+	-- Gain LP during Standby Phase
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e6:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e6:SetCountLimit(1)
+	e6:SetCondition(s.lpcon)
+	e6:SetOperation(s.lpop)
+	Duel.RegisterEffect(e6,tp)
+end
+function s.atkcon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttackPos),e:GetHandlerPlayer(),LOCATION_MZONE,0,2,nil)
+end
+function s.atklimit(e,c)
+	return c~=e:GetHandlerPlayer() and c:IsAttackPos() and c:GetAttack()~=Duel.GetFieldGroup(e:GetHandlerPlayer(),LOCATION_MZONE,0):GetMaxGroup(Card.GetAttack):GetFirst():GetAttack()
+end
+function s.indct(e,re,r,rp)
+	if (r&REASON_BATTLE+REASON_EFFECT)~=0 then
+		return 1
+	else 
+		return 0 
+	end
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and ev>0
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+function s.lpcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)
+	if ct>0 then
+		Duel.Recover(tp,ct*500,REASON_EFFECT)
+	end
+end
---
+++ script/c12345616.lua
@@ -0,0 +1,206 @@
+-- Fusion Monster Script
+local s, id = GetID()
+function s.initial_effect(c)
+  -- Fusion material requirement
+  c:EnableReviveLimit()
+  Fusion.AddProcMixN(c,true,true,s.ffilter,12)
+
+  -- Cannot be Special Summon negated
+  local e0 = Effect.CreateEffect(c)
+  e0:SetType(EFFECT_TYPE_SINGLE)
+  e0:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+  e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  c:RegisterEffect(e0)
+
+  -- Banish all other cards
+  local e1 = Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+  e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+  e1:SetProperty(EFFECT_FLAG_DELAY)
+  e1:SetCondition(s.banishcon)
+  e1:SetTarget(s.banishtg)
+  e1:SetOperation(s.banishop)
+  c:RegisterEffect(e1)
+
+  -- Unaffected by other card effects
+  local e2 = Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE)
+  e2:SetCode(EFFECT_IMMUNE_EFFECT)
+  e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  e2:SetRange(LOCATION_MZONE)
+  e2:SetValue(s.efilter)
+  c:RegisterEffect(e2)
+
+  -- Cannot be tributed or used as material
+  local e3 = Effect.CreateEffect(c)
+  e3:SetType(EFFECT_TYPE_SINGLE)
+  e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+  e3:SetValue(1)
+  c:RegisterEffect(e3)
+  local e4 = e3:Clone()
+  e4:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+  c:RegisterEffect(e4)
+  local e5 = e3:Clone()
+  e5:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+  c:RegisterEffect(e5)
+  local e6 = e3:Clone()
+  e6:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+  c:RegisterEffect(e6)
+  local e7 = e3:Clone()
+  e7:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+  c:RegisterEffect(e7)
+  local e8 = e3:Clone()
+  e8:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+  c:RegisterEffect(e8)
+
+  -- Cannot be destroyed by battle or card effects
+  local e9=Effect.CreateEffect(c)
+  e9:SetType(EFFECT_TYPE_SINGLE)
+  e9:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+  e9:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+  e9:SetRange(LOCATION_MZONE)
+  e9:SetValue(1)
+  c:RegisterEffect(e9)
+  local e10=e9:Clone()
+  e10:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+  c:RegisterEffect(e10)
+
+  -- Reflect battle damage and inflict damage
+  local e11 = Effect.CreateEffect(c)
+  e11:SetType(EFFECT_TYPE_CONTINUOUS + EFFECT_TYPE_SINGLE)
+  e11:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+  e11:SetCondition(s.damcon)
+  e11:SetOperation(s.damop)
+  c:RegisterEffect(e11)
+
+  -- Banish the monster after damage calculation
+  local e12 = Effect.CreateEffect(c)
+  e12:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+  e12:SetCode(EVENT_DAMAGE_STEP_END)
+  e12:SetCondition(s.banishcon2)
+  e12:SetOperation(s.banishop2)
+  c:RegisterEffect(e12)
+
+  -- Cannot lose the duel
+  local e13 = Effect.CreateEffect(c)
+  e13:SetType(EFFECT_TYPE_FIELD)
+  e13:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+  e13:SetCode(EFFECT_CANNOT_LOSE_LP)
+  e13:SetRange(LOCATION_MZONE)
+  e13:SetTargetRange(1, 0)
+  e13:SetValue(1)
+  c:RegisterEffect(e13)
+  
+  local e14 = Effect.CreateEffect(c)
+  e14:SetType(EFFECT_TYPE_FIELD)
+  e14:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+  e14:SetCode(EFFECT_CANNOT_LOSE_DECK)
+  e14:SetRange(LOCATION_MZONE)
+  e14:SetTargetRange(1, 0)
+  e14:SetValue(1)
+  c:RegisterEffect(e14)
+  
+  local e15 = Effect.CreateEffect(c)
+  e15:SetType(EFFECT_TYPE_FIELD)
+  e15:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+  e15:SetCode(EFFECT_CANNOT_LOSE_EFFECT)
+  e15:SetRange(LOCATION_MZONE)
+  e15:SetTargetRange(1, 0)
+  e15:SetValue(1)
+  c:RegisterEffect(e15)
+  
+  -- Special summon a monster from opponent's banish zone
+  local e16 = Effect.CreateEffect(c)
+  e16:SetType(EFFECT_TYPE_QUICK_O)
+  e16:SetCode(EVENT_FREE_CHAIN)
+  e16:SetRange(LOCATION_MZONE)
+  e16:SetCountLimit(1)
+  e16:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e16:SetProperty(EFFECT_FLAG_CARD_TARGET)
+  e16:SetTarget(s.ss_target)
+  e16:SetOperation(s.ss_operation)
+  c:RegisterEffect(e16)
+end
+
+function s.ffilter(c,fc,sub,mg,sg)
+  return c:IsCode(78371393) or (c:IsType(TYPE_MONSTER) and (not sg or not sg:IsExists(Card.IsLevel, 1, c:GetLevel())))
+end
+
+function s.banishcon(e,tp,eg,ep,ev,re,r,rp)
+  return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+
+function s.banishtg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return true end
+  Duel.SetOperationInfo(0, CATEGORY_REMOVE, nil, 0, PLAYER_ALL, LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+end
+
+function s.banishop(e,tp,eg,ep,ev,re,r,rp)
+  local c = e:GetHandler()
+  if not c:IsRelateToEffect(e) then return end
+  local g = Duel.GetFieldGroup(tp, LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE, LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+  if g:IsContains(c) then
+    g:RemoveCard(c)
+  end
+  Duel.Remove(g, POS_FACEUP, REASON_EFFECT)
+end
+
+function s.efilter(e, re)
+  return e:GetHandlerPlayer() ~= re:GetOwnerPlayer()
+end
+
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+  local c = e:GetHandler()
+  local tc = Duel.GetAttackTarget()
+  return (c == Duel.GetAttacker() or c == Duel.GetAttackTarget()) and c:IsRelateToBattle() and tc
+end
+
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+  Duel.ChangeBattleDamage(tp, 0)
+  Duel.ChangeBattleDamage(1-tp, ev, false)
+end
+
+function s.banishcon2(e,tp,eg,ep,ev,re,r,rp)
+  local c = e:GetHandler()
+  local atk = Duel.GetAttacker()
+  local tgt = Duel.GetAttackTarget()
+  -- Check if the card is either the attacker or the attack target
+  return (c == atk or c == tgt) and c:IsRelateToBattle()
+end
+
+function s.banishop2(e,tp,eg,ep,ev,re,r,rp)
+  local c = e:GetHandler()
+  local atk = Duel.GetAttacker()
+  local tgt = Duel.GetAttackTarget()
+  local tc = (c == atk) and tgt or atk
+
+  if tc and tc:GetControler() ~= tp then
+    -- Inflict damage equal to the attack of the monster to be banished
+    local damage = tc:GetAttack()
+    Duel.Damage(1-tp, damage, REASON_EFFECT)
+
+    -- Banish the monster
+    Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)
+  end
+end
+
+function s.ss_filter(c)
+  return c:IsType(TYPE_MONSTER)
+end
+
+function s.ss_target(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(s.ss_filter,tp,0,LOCATION_REMOVED,1,nil) end
+  Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+  local g = Duel.GetMatchingGroup(s.ss_filter,tp,0,LOCATION_REMOVED,nil)
+  local tc = g:Select(tp,1,1,nil):GetFirst()
+  Duel.SetTargetCard(tc)
+  Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, tc, 1, 0, 0)
+end
+
+function s.ss_operation(e,tp,eg,ep,ev,re,r,rp)
+  local tc = Duel.GetFirstTarget()
+  if tc and tc:IsRelateToEffect(e) then
+    Duel.SpecialSummon(tc, 0, 1-tp, 1-tp, false, false, POS_FACEUP)
+  end
+end
