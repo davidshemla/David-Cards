6b9ff8ed9cccdaa996ae0de117555e9effad0134
davidshemla <davidshemla2501@gmail.com>
2024-08-11 08:40:51 +0300

    h

--- script/c12345612.lua
+++ script/c12345612.lua
@@ -2,33 +2,40 @@
 local s,id=GetID()
 function s.initial_effect(c)
     -- Special Summon up to 5 copies of the specified monsters
-    local e1 = Effect.CreateEffect(c)
+    local e1=Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetCountLimit(1)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 end
 s.listed_names={6007213,32491822,69890967}
 
--- Define the target function
-function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return true end
-    local g = Duel.GetMatchingGroup(function(c) return c:IsCode(6007213, 32491822, 69890967) end, tp, LOCATION_HAND + LOCATION_DECK, 0, nil)
-    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, 5, 0, 0)
+function s.spfilter(c,e,tp)
+    return c:IsCode(6007213,32491822,69890967) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 
--- Define the operation function
-function s.spop(e, tp, eg, ep, ev, re, r, rp)
-    local g = Duel.GetMatchingGroup(function(c) return c:IsCode(6007213, 32491822, 69890967) end, tp, LOCATION_HAND + LOCATION_DECK, 0, nil)
-    local tc = g:GetFirst()
-    local count = 0
-    while tc and count < 5 do
-        Duel.SpecialSummon(tc, 0, tp, tp, true, false, POS_FACEUP_ATTACK)
-        count = count + 1
-        tc = g:GetNext()
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return false end
+    if chk==0 then 
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) 
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+        local max_targets=math.min(Duel.GetLocationCount(tp,LOCATION_MZONE),5)
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,max_targets,nil,e,tp)
+        if #g>0 then
+            for tc in aux.Next(g) do
+                Duel.SpecialSummonStep(tc,0,tp,tp,true,false,POS_FACEUP_ATTACK)
+                tc:CompleteProcedure()
+            end
+            Duel.SpecialSummonComplete()
+        end
     end
 end
