61d2aabadf4b3b9bcaa07400ac8badb66d58d6a6
davidshemla <davidshemla2501@gmail.com>
2024-10-25 14:28:41 +0300

    Update c12345641.lua

--- script/c12345641.lua
+++ script/c12345641.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 
     -- Double ATK
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_QUICK_F)
+    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
     e2:SetRange(LOCATION_SZONE)
     e2:SetTarget(s.target)
